85ba8a2f96936c011026ac1c88754de3aa893874
ImSeanStaAna10 <seanstaana0510@gmail.com>
2026-07-18 02:52:52 +0800

    added login method in authservice applying password hashing using BCrypt package

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,5 +1,6 @@
 ﻿using LoginAuthAPI.Contracts;
 using LoginAuthAPI.DTO_S;
+using LoginAuthAPI.Entities;
 
 namespace LoginAuthAPI.Services
 {
@@ -14,12 +15,71 @@ namespace LoginAuthAPI.Services
 
         public AuthResponseDTO Register(RegisterRequestDTO request)
         {
-            throw new NotImplementedException();
+            var existingUser = _userRepository.GetByEmail(request.Email);
+
+            if (existingUser != null)
+            {
+                return new AuthResponseDTO
+                {
+                    Message = "Email Already Exist"
+                };
+            }
+
+            //MAP DTO to Entity
+            var user = new User
+            {
+                FullName = request.FullName,
+                Email = request.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+                Role = "User"
+
+            }; 
+
+            //Add and Save User
+            _userRepository.AddUser(user);
+            _userRepository.SaveChanges();
+
+            return new AuthResponseDTO
+            {
+                Message = "User Registered Succesfull"
+            };
         }
 
         public AuthResponseDTO Login(LoginRequestDTO request)
         {
-            throw new NotImplementedException();
+            var user = _userRepository.GetByEmail(request.Email);
+
+            if (user == null)
+            {
+                return new AuthResponseDTO
+                {
+                    Message = "Invalid Email Or Password"
+                };
+
+            }
+
+
+            //Verify Pass
+            bool isPasswordValid = BCrypt.Net.BCrypt.Verify(
+                request.Password,
+                user.PasswordHash
+                );
+
+            if(!isPasswordValid)
+            {
+                return new AuthResponseDTO
+                {
+                    Message = "Invalid Email Or Password"
+                };
+
+            };
+
+            return new AuthResponseDTO
+            {
+                Message = "Login Succesfull"
+            };
+
+
         }
     }
 }
